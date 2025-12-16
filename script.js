3014a2fd6c6c82ad73d86c4385de3288c8408edb
Abelion07 <151552607+Abelion07@users.noreply.github.com>
2025-12-16 13:30:35 +0100

    design

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 import { App } from "./components/app.js";
 const root = document.querySelector("#root");
 root.appendChild(App());
+
+function lekeres() {
+  return fetch("http://localhost:3001/api/users/3/transactions").then((r) =>
+    r.json()
+  );
+}
+
+lekeres().then((data) => console.log("adat:", data));
